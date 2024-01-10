3d25d8b269f30fda5ff352126d21cc72b01452df
Jeffrey Scott <82685075+vader9911@users.noreply.github.com>
2024-01-09 19:01:55 -0500

    add questions/template for inquirer

---
+++ index.js
@@ -0,0 +1,68 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+      },
+
+
+
+    //"Please enter your project's title:",                   //0
+    // "Please enter a description of your project:",          //1
+    // "Please enter this README's table of contents:",        //2
+    // "Please describe your project's installation:",         //3
+    // "Please describe your project's usage:",                //4
+    // "Please enter your project's license:",                 //5
+    // "Please enter your project's contributions:",           //6
+    // "Please describe your project's tests:",                //7
+    // "Please describe your project's questions:"             //8
+];
+//Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
